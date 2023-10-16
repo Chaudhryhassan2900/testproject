bc99346cd8bc28dce8612c68207539ee64bfb01d
Chaudhryhassan2900 <chaudhryhassan2900@gmail.com>
2023-10-16 18:43:54 +0500

    setting up account

--- hom_frontend/src/App.js
+++ hom_frontend/src/App.js
@@ -2,7 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import MiniDrawer from './Component/Admin/MiniDrawer';
 import Dashboard from './Component/Dashboard/Dashboard';
-
+//setting up git account
 function App() {
   return (
     <div className="App" >
